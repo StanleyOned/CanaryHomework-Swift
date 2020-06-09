947892ca3fc993cc90a806c1a63585140b8fdc61
Stanley Delacruz <goodiie20@gmail.com>
2020-06-09 09:42:53 -0400

    Initial Commit

---
+++ CanaryHomework-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CanaryHomework-Swift
+//
+//  Created by Stanley Delacruz on 6/9/20.
+//  Copyright © 2020 Stanle Delacruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
